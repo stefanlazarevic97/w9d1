66635dae44b91287a03db1636fc06f1ba7e94252
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-17 12:01:03 -0700

    add mo circle test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 console.log("Webpack is working!")
+
+import MovingObject from "./moving-object.js";
+
+document.addEventListener("DOMContentLoaded", function() {
+    const canvas = document.getElementById("game-canvas");
+    const ctx = canvas.getContext("2d");
+    window.ctx = ctx;
+
+    const mo = new MovingObject({
+        pos: [30, 30],
+        vel: [10, 10],
+        radius: 5,
+        color: "#00FF00"
+    });
+
+    window.mo = mo;
+})
+
+window.MovingObject = MovingObject;
---
+++ src/moving-object.js
@@ -0,0 +1,17 @@
+class MovingObject {
+    constructor(options) {
+        this.pos = options.pos;
+        this.vel = options.vel;
+        this.radius = options.radius;
+        this.color = options.color;
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI * 2, true);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+}
+
+export default MovingObject;
