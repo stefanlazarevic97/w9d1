00c35106f03a0b09705164500d1bb4c69bbfd078
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-17 12:17:40 -0700

    add move function to MovingObject

--- src/moving-object.js
+++ src/moving-object.js
@@ -12,6 +12,11 @@ class MovingObject {
         ctx.fillStyle = this.color;
         ctx.fill();
     }
+
+    move() {
+        this.pos[0] += this.vel[0];
+        this.pos[1] += this.vel[1];
+    }
 }
 
 export default MovingObject;
